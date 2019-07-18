45ded686cec287b1d2efffaa8e7c13c99f2480f7
Jesse.Ssempijja <jesse.ssempijja@postbank.co.ug>
2019-07-18 15:11:25 +0300

    finished testing and CI, as well as HTTP logic

--- src/test.js
+++ src/test.js
@@ -1,174 +1,174 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
-// const supportedCards = {
-//   visa, mastercard
+// // const supportedCards = {
+// //   visa, mastercard
+// // };
+
+// const appState = {};
+
+
+// const detectCardType = ({ target }) => {
+
+
 // };
 
-const appState = {};
+// const validateCardExpiryDate = ({ target }) => {
+//     const date = new Date;
+//     const validFormat = expiryDateFormatIsValid(target.value);
+//     const expiry = new Date(target.value);
+//     const dateValid = validFormat && expiry >= date;
+//     return dateValid && flagIfInvalid(target, dateValid);
 
 
-const detectCardType = ({ target }) => {
+// };
+
+// const validateCardHolderName = ({ target }) => {
+//     const format = '/^.{2,}([a-zA-Z]+ ){1}[a-zA-Z]+\w{2,}$/';
+//     const name = target.value;
+
+//     if (name.match(format)) {
+//         flagIfInvalid(name, true);
+//         return true;
+//     } else {
+//         flagIfInvalid(name, false);
+//         return false;
+//     }
 
 
-};
+// };
 
-const validateCardExpiryDate = ({ target }) => {
-    const date = new Date;
-    const validFormat = expiryDateFormatIsValid(target.value);
-    const expiry = new Date(target.value);
-    const dateValid = validFormat && expiry >= date;
-    return dateValid && flagIfInvalid(target, dateValid);
+// const uiCanInteract = _ => {
+//     const inputs_digits = document.querySelectorAll('[data-cc-digits] > input');
+//     const inputs_info = document.querySelectorAll('[data-cc-info] >input');
+//     const btn = document.querySelector([data - pay - btn]);
 
+//     inputs_digits[0].addEventListener('blur', detectCardType, false);
+//     console.log('#1 ' + inputs_digits[0].value);
+//     inputs_info[0].addEventListener('blur', validateCardHolderName, false);
+//     inputs_info[1].addEventListener('blur', validateCardExpiryDate, false);
 
-};
-
-const validateCardHolderName = ({ target }) => {
-    const format = '/^.{2,}([a-zA-Z]+ ){1}[a-zA-Z]+\w{2,}$/';
-    const name = target.value;
-
-    if (name.match(format)) {
-        flagIfInvalid(name, true);
-        return true;
-    } else {
-        flagIfInvalid(name, false);
-        return false;
-    }
+//     btn.addEventListener('click', validateCardNumber, false);
+//     inputs_digits[0].focus();
 
 
-};
+// };
 
-const uiCanInteract = _ => {
-    const inputs_digits = document.querySelectorAll('[data-cc-digits] > input');
-    const inputs_info = document.querySelectorAll('[data-cc-info] >input');
-    const btn = document.querySelector([data - pay - btn]);
+// const displayCartTotal = ({ results }) => {
+//     let [data] = results;
+//     const { itemsInCart, buyerCountry } = data;
+//     appState.items = itemsInCart;
+//     appState.country = buyerCountry;
+//     appState.bill = itemsInCart.reduce((total, current) => {
+//         return total + (current.price * current.qty);
+//     }, 0);
+//     appState.billFormatted = formatAsMoney(appState.bill, appState.country);
 
-    inputs_digits[0].addEventListener('blur', detectCardType, false);
-    console.log('#1 ' + inputs_digits[0].value);
-    inputs_info[0].addEventListener('blur', validateCardHolderName, false);
-    inputs_info[1].addEventListener('blur', validateCardExpiryDate, false);
+//     let span = document.querySelector('[data-bill]');
+//     span.textContent = appState.billFormatted;
 
-    btn.addEventListener('click', validateCardNumber, false);
-    inputs_digits[0].focus();
-
-
-};
-
-const displayCartTotal = ({ results }) => {
-    let [data] = results;
-    const { itemsInCart, buyerCountry } = data;
-    appState.items = itemsInCart;
-    appState.country = buyerCountry;
-    appState.bill = itemsInCart.reduce((total, current) => {
-        return total + (current.price * current.qty);
-    }, 0);
-    appState.billFormatted = formatAsMoney(appState.bill, appState.country);
-
-    let span = document.querySelector('[data-bill]');
-    span.textContent = appState.billFormatted;
-
-    uiCanInteract();
+//     uiCanInteract();
 
 
 
-};
+// };
 
-const fetchBill = () => {
-    const api = 'https://randomapi.com/api/006b08a801d82d0c9824dcfdfdfa3b3c';
+// const fetchBill = () => {
+//     const api = 'https://randomapi.com/api/006b08a801d82d0c9824dcfdfdfa3b3c';
 
-    fetch(api).then(response => {
-        return response.json();
-    }).then(displayCartTotal).catch(err => console.error(err));
+//     fetch(api).then(response => {
+//         return response.json();
+//     }).then(displayCartTotal).catch(err => console.error(err));
 
-}
+// }
 
 
 
 
-const countries = [{
-        code: "US",
-        currency: "USD",
-        country: 'United States'
-    },
-    {
-        code: "NG",
-        currency: "NGN",
-        country: 'Nigeria'
-    },
-    {
-        code: 'KE',
-        currency: 'KES',
-        country: 'Kenya'
-    },
-    {
-        code: 'UG',
-        currency: 'UGX',
-        country: 'Uganda'
-    },
-    {
-        code: 'RW',
-        currency: 'RWF',
-        country: 'Rwanda'
-    },
-    {
-        code: 'TZ',
-        currency: 'TZS',
-        country: 'Tanzania'
-    },
-    {
-        code: 'ZA',
-        currency: 'ZAR',
-        country: 'South Africa'
-    },
-    {
-        code: 'CM',
-        currency: 'XAF',
-        country: 'Cameroon'
-    },
-    {
-        code: 'GH',
-        currency: 'GHS',
-        country: 'Ghana'
-    }
-];
+// const countries = [{
+//         code: "US",
+//         currency: "USD",
+//         country: 'United States'
+//     },
+//     {
+//         code: "NG",
+//         currency: "NGN",
+//         country: 'Nigeria'
+//     },
+//     {
+//         code: 'KE',
+//         currency: 'KES',
+//         country: 'Kenya'
+//     },
+//     {
+//         code: 'UG',
+//         currency: 'UGX',
+//         country: 'Uganda'
+//     },
+//     {
+//         code: 'RW',
+//         currency: 'RWF',
+//         country: 'Rwanda'
+//     },
+//     {
+//         code: 'TZ',
+//         currency: 'TZS',
+//         country: 'Tanzania'
+//     },
+//     {
+//         code: 'ZA',
+//         currency: 'ZAR',
+//         country: 'South Africa'
+//     },
+//     {
+//         code: 'CM',
+//         currency: 'XAF',
+//         country: 'Cameroon'
+//     },
+//     {
+//         code: 'GH',
+//         currency: 'GHS',
+//         country: 'Ghana'
+//     }
+// ];
 
-const formatAsMoney = (amount, buyerCountry) => {
-    let country = buyerCountry;
-    let countryobj = arr => arr.filter((country, index) => {
-        if (arr[index].country === buyerCountry) {
-            return {
-                code: arr[index].code,
-                currency: arr[index].currency
-            };
-        }
-    });
+// const formatAsMoney = (amount, buyerCountry) => {
+//     let country = buyerCountry;
+//     let countryobj = arr => arr.filter((country, index) => {
+//         if (arr[index].country === buyerCountry) {
+//             return {
+//                 code: arr[index].code,
+//                 currency: arr[index].currency
+//             };
+//         }
+//     });
 
-    if (countryobj(countries).length != 0) {
-        return amount.toLocaleString(countryobj(countries)[0].code, { style: 'currency', currency: countryobj(countries)[0].currency });
-    } else {
-        return amount.toLocaleString('US', { style: 'currency', currency: 'USD' });
-    }
+//     if (countryobj(countries).length != 0) {
+//         return amount.toLocaleString(countryobj(countries)[0].code, { style: 'currency', currency: countryobj(countries)[0].currency });
+//     } else {
+//         return amount.toLocaleString('US', { style: 'currency', currency: 'USD' });
+//     }
 
 
-};
+// };
 
-const flagIfInvalid = (field, isValid) => {
-    if (isValid) {
-        field.classList.remove('is-invalid');
-    } else {
-        field.classList.add('is-invalid');
-    }
-};
+// const flagIfInvalid = (field, isValid) => {
+//     if (isValid) {
+//         field.classList.remove('is-invalid');
+//     } else {
+//         field.classList.add('is-invalid');
+//     }
+// };
 
-const expiryDateFormatIsValid = (target) => {
-    let format = '/^\d{2}\/\d{2}$/';
-    if (target.match(format)) { return true } else {
-        return false;
-    }
+// const expiryDateFormatIsValid = (target) => {
+//     let format = '/^\d{2}\/\d{2}$/';
+//     if (target.match(format)) { return true } else {
+//         return false;
+//     }
 
-}
+// }
 
-const startApp = () => {
-    fetchBill();
-};
+// const startApp = () => {
+//     fetchBill();
+// };
 
-startApp();
+// startApp();
